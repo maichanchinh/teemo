929825b75be99360c09e68640b8c4c2951531c97
quanhh <quanhh@hiworld.com.vn>
2015-12-04 16:10:52 +0700

    backend layout 2

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,6 +4,7 @@
 Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( bootstrap.min.css )
 Rails.application.config.assets.precompile += %w( default.css )
+Rails.application.config.assets.precompile += %w( default-admin.css )
 Rails.application.config.assets.precompile += %w( flat-ui.min.css )
 Rails.application.config.assets.precompile += %w( bootstrap.min.js )
 Rails.application.config.assets.precompile += %w( jquery-2.1.4.min.js )
